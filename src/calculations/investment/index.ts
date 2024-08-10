11b64df912ca63e8ef3f4929257b5443144bbe43
Abhaya Shankar <shankarabhaya9@gmail.com>
2024-08-10 08:38:49 +0530

    feat(loan-calculators): basic loan calculators - develop

--- src/calculations/investment/index.ts
+++ src/calculations/investment/index.ts
@@ -1,7 +1,3 @@
 export * from "./sip";
 export * from "./compoundInterest";
 export * from "./simpleInterest";
-export * from "./homeLoan";
-export * from "./personalLoan";
-export * from "./businessLoan";
-export * from "./educationLoan";
